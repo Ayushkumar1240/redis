db5c85540f8776b7ff3c7346560faa2ac58b3fd1
Ayushkumar1240 <ayush.kumar@dsgroup.com>
2024-11-26 17:50:49 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const axios = require('axios').default
+const client = require('./client')
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const cache = await client.get('todos');
+    if (cache) {
+        return res.json(JSON.parse(cache));
+    }
+
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos')
+    await client.set('todos',JSON.stringify(data));
+    await client.expire('todos',30);
+    return res.json(data);
+ })
+
+app.listen(9000);
